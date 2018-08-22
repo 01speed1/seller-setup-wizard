5367795d21a6aa54bd6b444eeb4a575bd923b022
Oscar Guzman <o1speed1@hotmail.com>
2018-08-22 11:22:38 -0500

    Se hizo cambio de colores a morado y azul

--- seller-setup-wizard.php
+++ seller-setup-wizard.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Seller setup wizard class
@@ -120,44 +121,49 @@ class Dokan_Seller_Setup_Wizard extends Dokan_Setup_Wizard {
                 .wc-setup-steps {
                     justify-content: center;
                 }
+
+		/*color por defecto: naranja principal  #f39132 por #140A47  */
+		/*naranja sombra #ff6b00*/
+
+		/*Morado brillante #a36597*/
                 .wc-setup-content a {
-                    color: #f39132;
+                    color: #140A47; /*cambio de color*/
                 }
                 .wc-setup-steps li.active:before {
-                    border-color: #f39132;
+                    border-color: #140A47; /*cambio de color*/
                 }
                 .wc-setup-steps li.active {
-                    border-color: #f39132;
-                    color: #f39132;
+                    border-color: #140A47; /*cambio de color*/
+                    color: #140A47; /*cambio de color*/
                 }
                 .wc-setup-steps li.done:before {
-                    border-color: #f39132;
+                    border-color: #140A47; /*cambio de color*/
                 }
                 .wc-setup-steps li.done {
-                    border-color: #f39132;
-                    color: #f39132;
+                    border-color: #140A47; /*cambio de color*/
+                    color: #140A47; /*cambio de color*/
                 }
                 .wc-setup .wc-setup-actions .button-primary, .wc-setup .wc-setup-actions .button-primary, .wc-setup .wc-setup-actions .button-primary {
-                    background: #f39132 !important;
+                    background: #140A47 !important; /*cambio de color*/
                 }
                 .wc-setup .wc-setup-actions .button-primary:active, .wc-setup .wc-setup-actions .button-primary:focus, .wc-setup .wc-setup-actions .button-primary:hover {
-                    background: #ff6b00 !important;
-                    border-color: #ff6b00 !important;
+                    background: #2061BB !important; /*cambio de color azul claro */
+                    border-color: #2061BB !important; /*cambio de color azul claro */
                 }
                 .wc-setup-content .wc-setup-next-steps ul .setup-product a, .wc-setup-content .wc-setup-next-steps ul .setup-product a, .wc-setup-content .wc-setup-next-steps ul .setup-product a {
-                    background: #f39132 !important;
-                    box-shadow: inset 0 1px 0 rgba(255,255,255,.25),0 1px 0 #f39132;
+                    background: #140A47 !important; /*cambio de color*/ 
+                    box-shadow: inset 0 1px 0 rgba(255,255,255,.25),0 1px 0 #140A47; /*cambio de color*/
                 }
                 .wc-setup-content .wc-setup-next-steps ul .setup-product a:active, .wc-setup-content .wc-setup-next-steps ul .setup-product a:focus, .wc-setup-content .wc-setup-next-steps ul .setup-product a:hover {
-                    background: #ff6b00 !important;
-                    border-color: #ff6b00 !important;
-                    box-shadow: inset 0 1px 0 rgba(255,255,255,.25),0 1px 0 #ff6b00;
+                    background:  #2061BB !important; /*cambio de color azul claro */
+                    border-color:  #2061BB !important; /*cambio de color azul claro */
+                    box-shadow: inset 0 1px 0 rgba(255,255,255,.25),0 1px 0 #2061bb; /*cambio de color azul claro*/
                 }
                 .wc-setup .wc-setup-actions .button-primary {
-                    border-color: #f39132 !important;
+                    border-color:  #a36597 !important; /*cambio de color morado brillante  */ 
                 }
                 .wc-setup-content .wc-setup-next-steps ul .setup-product a {
-                    border-color: #f39132 !important;
+                    border-color:  #2061BB !important; /*cambio de color azul claro */
                 }
                 ul.wc-wizard-payment-gateways li.wc-wizard-gateway .wc-wizard-gateway-enable input:checked+label:before {
                     background: #f39132 !important;
@@ -267,7 +273,10 @@ class Dokan_Seller_Setup_Wizard extends Dokan_Setup_Wizard {
                     <th scope="row"><label for="address[country]"><?php _e( 'Country', 'dokan-lite' ); ?> *</label></th>
                     <td>
                         <select name="address[country]" class="wc-enhanced-select country_to_state" id="address[country]" required>
-                            <?php dokan_country_dropdown( $countries, $address_country, false ); ?>
+                            <!-- probando dejar a colombia como pais predeterminado-->
+				<option id="selected_country" value="CO" selected="selected">Colombia</option>
+			   <!-- fin test option defualt-->
+			   <?php dokan_country_dropdown( $countries, $address_country, false ); ?>
                         </select>
                     </td>
                 </tr>
